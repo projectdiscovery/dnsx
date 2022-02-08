9b695dac2cc2893c710d6e3f2ab41ef3474a8d78
mzack <marco.rivoli.nvh@gmail.com>
2022-02-08 10:32:02 +0100

    updating run.sh files

--- cmd/functional-test/run.sh
+++ cmd/functional-test/run.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo 'Building functional-test binary'
-go build
+# reading os type from arguments
+CURRENT_OS=$1
 
-echo 'Building DNSX binary from current branch'
-go build -o dnsx_dev ../dnsx
+if [ "${CURRENT_OS}" == "windows-latest" ];then
+    extension=.exe
+fi
 
-echo 'Installing latest release of DNSX'
-GO111MODULE=on go build -v github.com/projectdiscovery/dnsx/cmd/dnsx
+echo "::group::Building functional-test binary"
+go build -o functional-test$extension
+echo "::endgroup::"
 
-echo 'Starting DNSX functional test'
-./functional-test -main ./dnsx -dev ./dnsx_dev -testcases testcases.txt
+echo "::group::Building dnsx binary from current branch"
+go build -o dnsx_dev$extension ../dnsx
+echo "::endgroup::"
+
+echo "::group::Building latest release of dnsx"
+go build -o dnsx$extension -v github.com/projectdiscovery/dnsx/cmd/dnsx
+echo "::endgroup::"
+
+echo 'Starting dnsx functional test'
+./functional-test$extension -main ./dnsx$extension -dev ./dnsx_dev$extension -testcases testcases.txt
--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "::group::Build dnsx"
 rm integration-test dnsx 2>/dev/null
 cd ../cmd/dnsx
 go build
 mv dnsx ../../integration_tests/dnsx
+echo "::endgroup::"
+echo "::group::Build nuclei integration-test"
 cd ../integration-test
 go build
 mv integration-test ../../integration_tests/integration-test 
 cd ../../integration_tests
+echo "::endgroup::"
 ./integration-test
 if [ $? -eq 0 ]
 then
